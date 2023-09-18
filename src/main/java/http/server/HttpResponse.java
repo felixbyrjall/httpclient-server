882a766d7e7d6e4467fa1274279c93c88babd3f6
Felix Byrjall <felixbyrjall@gmail.com>
2023-09-18 23:38:37 +0200

    added httpresponse.java

--- src/main/java/http/server/HttpResponse.java
+++ src/main/java/http/server/HttpResponse.java
@@ -6,15 +6,37 @@ import java.util.Map;
 
 public class HttpResponse {
 
-    public final String statusLine;
+    public String statusLine;
     public final Map<String, String> headers;
-    public final String body;
+    public String body;
 
     public HttpResponse(HttpRequest request) {
         headers = new HashMap<>();
-        body = "<html><h1>Hello world!</h1></html>";
-        statusLine = "HTTP/1.1 200 OK\r\n";
-        headers.put("Content-Length", body.getBytes(StandardCharsets.UTF_8).length + "\r\n");
-        headers.put("Content-Type", "text/html\r\n");
+        if (request.getMethod().equals("GET")) {
+            if (request.getResource().equals("/")) {
+                body = "<html><h1>Hello world!</h1></html>";
+                statusLine = "HTTP/1.1 200 OK\r\n";
+                headers.put("Content-Length", body.getBytes(StandardCharsets.UTF_8).length + "\r\n");
+                headers.put("Content-Type", "text/html\r\n");
+            } else if (request.getResource().equals("/cat/flavorcountry")) {
+                body = "<html><h1>Cats only come in one flavor</h1></html>";
+                statusLine = "HTTP/1.1 200 OK\r\n";
+                headers.put("Content-Length", body.getBytes(StandardCharsets.UTF_8).length + "\r\n");
+                headers.put("Content-Type", "text/html\r\n");
+            } else {
+                body = "<html><h1>404 NOT FOUND</h1></html>";
+                statusLine = "HTTP/1.1 404 NOT FOUND\r\n";
+            }
+        } else if (request.getMethod().equals("POST")) {
+            body = "<html><h1>POST</h1></html>";
+            statusLine = "HTTP/1.1 200 OK\r\n";
+            headers.put("Content-Length", body.getBytes(StandardCharsets.UTF_8).length + "\r\n");
+            headers.put("Content-Type", "text/html\r\n");
+        } else if (request.getResource().equals("/") && request.getMethod().equals("POST")) {
+            body = "<html><h1>405</h1></html>";
+            statusLine = "HTTP/1.1 405\r\n";
+            headers.put("Content-Length", body.getBytes(StandardCharsets.UTF_8).length + "\r\n");
+            headers.put("Content-Type", "text/html\r\n");
+        }
     }
 }
